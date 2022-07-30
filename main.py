9ceede26c3f0cf613ba2a3a4e7be68d191229633
legal justus <justus@daccs.nl>
2022-07-30 17:22:55 +0200

    data processing done

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ df = pd.read_csv('Churn.csv')
 X = pd.get_dummies(df.drop(['Churn','Customer ID'], axis=1))
 y = df['Churn'].apply(lambda x: 1  if x =='Yes' else 0)
 
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=.2)
+
+y_train.head()
+
